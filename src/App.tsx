200c78d7043d4123c1b36cb4631705718a547bfe
VladimirKaspy <vladimircvetkovic77@icloud.com>
2022-10-01 21:51:06 +0200

    Initial setup: CRA with Typescript, Antd with theming, Sass, Router
    The first thing I did was to create a new project with create-react-app,
    including Typescript, and then installed Antd and configure it with theming.
    I also installed Sass.
    Installed react-router-dom and configured few simple routes and
    layout component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/specific/Layout";
+import Home from "./pages/Home";
+import Properties from "./pages/Properties";
+import "./App.less";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/properties" element={<Properties />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
